27796931ab7ea7710f48c325042dbf45745128b7
Nishant2052 <nishantraj211999@gmail.com>
2026-08-17 23:56:34 +0530

    Added JWT Implementation

--- src/main/java/com/example/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurity/config/SecurityConfig.java
@@ -2,14 +2,26 @@ package com.example.SpringSecurity.config;
 
 import com.example.SpringSecurity.Entity.CustomUserDetails;
 import com.example.SpringSecurity.Service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
 @Configuration
 public class SecurityConfig {
@@ -28,16 +40,64 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, DaoAuthenticationProvider provider){
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, DaoAuthenticationProvider provider,JwtAuthenticationConverter jwtAuthenticationConverter){
 
         httpSecurity.csrf(csrf->csrf.disable())
                 .authenticationProvider(provider)
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
+//                .formLogin(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(auth->
                         auth.requestMatchers("/api/user/register").permitAll()
-                        .requestMatchers("/api/roles").permitAll()
-                                .anyRequest().authenticated());
+                        .requestMatchers("/api/roles","/auth/login").permitAll()
+                                .anyRequest().authenticated())
+                .sessionManagement(session->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oAuth2->oAuth2.jwt(jwt->jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
+
         return httpSecurity.build();
+    }
+
+    @Bean
+    public SecretKey jwtSecretKey(@Value("${jwt.secret}") String secret){
+     byte [] decodeKey= Base64.getDecoder().decode(secret);
+     return new SecretKeySpec(decodeKey,"HmacSHA256");
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(DaoAuthenticationProvider authenticationProvider){
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter(){
+        JwtGrantedAuthoritiesConverter authoritiesConverter=new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthoritiesClaimName("authorities");
+        authoritiesConverter.setAuthorityPrefix("");
+        JwtAuthenticationConverter authenticationConverter=new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+        return authenticationConverter;
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder( SecretKey secretKey){
+        return NimbusJwtEncoder.withSecretKey(secretKey).algorithm(MacAlgorithm.HS256).build();
+    }
+    @Bean
+    public JwtDecoder jwtDecoder(
+            SecretKey secretKey,
+            @Value("${jwt.issuer}") String issuer) {
+
+        NimbusJwtDecoder decoder =
+                NimbusJwtDecoder
+                        .withSecretKey(secretKey)
+                        .macAlgorithm(MacAlgorithm.HS256)
+                        .build();
+
+        decoder.setJwtValidator(JwtValidators.createDefaultWithIssuer(issuer
+                )
+        );
+
+        return decoder;
     }
 }
